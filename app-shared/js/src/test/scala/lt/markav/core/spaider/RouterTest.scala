c10454471514d5afd22ff1273567410174698741
Marius Kavoliūnas <kavoliunas.m@gmail.com>
2018-03-23 00:03:53 +0200

    tests in progress

--- app-shared/js/src/test/scala/lt/markav/core/spaider/RouterTest.scala
+++ app-shared/js/src/test/scala/lt/markav/core/spaider/RouterTest.scala
@@ -2,59 +2,82 @@ package lt.markav.core.spaider
 
 import java.util.UUID
 
+import lt.markav.core.spaider.TestWidget.buildBranch
 import minitest.SimpleTestSuite
 import org.scalajs.dom.Element
 
 import scalatags.JsDom
 import scalatags.JsDom.all._
+import scalatags.generic
+import scalatags.text.Builder
 
 object RouterTest extends SimpleTestSuite {
 
-  test("Routes To Home with empty path") {
+//  test("Routes to home widget with empty path") {
+//    val id = rndId
+//    implicit val widget: Widget = TestWidget(widgets = List(TestWidget(id)))
+//    assertEquals(Router.route(Path()).id, id)
+//  }
+
+//  test("Routes to home widget with 'home' path") {
+//    val path = "home"
+//    val id = rndId
+//    implicit val widget: Widget = TestWidget(widgets = List(TestWidget(id, path)))
+//    assertEquals(Router.route(path).id, id)
+//  }
+
+//  test("Routes to top widget witch match full path") {
+//    val id1 = rndId
+//    val id2 = rndId
+//    val fullPath = Path("one") / "two" / "three"
+//    implicit val widget: Widget = TestWidget(widgets = List(
+//      buildBranch(id1, "one", "two", "three"),
+//      buildBranch(id2, fullPath)
+//    ))
+//    assertEquals(Router.route(fullPath).id, id2)
+//  }
+
+  test("Routes to deep node of the branch") {
     val id = rndId
-
     implicit val widget: Widget = TestWidget(widgets = List(
-      TestWidget(id)
+      buildBranch(id, "one", "two", "three")
     ))
-
-    assertEquals(Router.route(Path()).id, id)
-  }
-
-  test("Routes To Home with home path") {
-    val path = "home"
-    val id = rndId
-
-    implicit val widget: Widget = TestWidget(widgets = List(
-      TestWidget(id, path)
-    ))
-
-    assertEquals(Router.route(Path(path)).id, id)
-  }
-
-  test("Routes To Home with home path") {
-    val path = "home"
-    val id = rndId
-
-    implicit val widget: Widget = TestWidget(widgets = List(
-      TestWidget(id, path)
-    ))
-
-    assertEquals(Router.route(Path(path)).id, "fail")
+    println(widget)
+    assertEquals(Router.route(Path("one") / "two" / "three").id, id)
   }
 
   /** */
 
   implicit class IdGetter(tag: JsDom.TypedTag[Element]) {
-    def id: String = tag.modifiers.head.head.asInstanceOf[StringFrag].v
+    def id: String = {
+      println(tag.modifiers)
+      type Attr = generic.AttrPair[Builder, String]
+      val attr = tag.modifiers.head.find(_.isInstanceOf[AttrPair]).map(_.asInstanceOf[Attr])
+      attr match {
+        case Some(a) => a.v
+        case None => "undefined"
+      }
+    }
   }
 
-  def rndId = UUID.randomUUID().toString
+  private def rndId = UUID.randomUUID().toString
 
 }
 
-case class TestWidget(id: String = "",
+case class TestWidget(wid: String = "",
                       override val path: Path = Path(),
-                      override val widgets: List[Widget] = List.empty,
-                     ) extends Widget {
-  override def contents: Tag = div(id)
+                      override val widgets: List[Widget] = List.empty)
+  extends Widget {
+  override def contents: Tag =
+    if (wid.isEmpty) div("")
+    else div(wid, page)
+}
+
+object TestWidget {
+
+  def buildBranch(id: String, paths: Path*): Widget =
+    paths.init.foldRight(TestWidget(id, paths.last))(
+      (in, out) => TestWidget(path = in, widgets = List(out))
+    )
+
 }
